dc946b1463ab23a46be225430d50d2bfac37cf43
neshamon <jmmatthews@proton.me>
2023-10-10 20:51:32 -0500

    Create failure case for expected types

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -22,6 +22,16 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
+	}
 
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+			i, tt.expectedType, tok.Type)
+		}
 	}
 }
