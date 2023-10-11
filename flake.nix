e71cf4825bd5e49b30fe73b990b8641053b62a42
neshamon <jmmatthews@proton.me>
2023-10-10 19:55:08 -0500

    Add easy version control to go

--- flake.nix
+++ flake.nix
@@ -3,14 +3,16 @@
 {
   description = "Nix flake for monkey-lang";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      goVersion = 21;
+      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
     in
     {
       devShells.x86_64-linux.default = (import ./shell.nix { inherit pkgs; });
