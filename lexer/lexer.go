769c9396281865f39a525c9034fa275c31cae84b
neshamon <jmmatthews@proton.me>
2023-10-10 21:37:57 -0500

    Create readChar func & add notes

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,7 +7,26 @@ type Lexer struct {
 	ch byte  // current char under examination
 }
 
+/* position & readPosition access characters in input by using the characters
+ as an index.
+
+   Why there are two positions is so both the input of the current
+   character and the subsequent one can be inspected. position describes the
+   current character and readPosition describes the next. ch represents the byte
+   of the current character in the input
+*/
+
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
 }
