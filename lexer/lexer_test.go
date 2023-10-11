5781d466e7f3920bb931e7fe1d20b9d76e3953cf
neshamon <jmmatthews@proton.me>
2023-10-10 21:05:47 -0500

    Fix typos & add literal fail case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -3,14 +3,14 @@ package lexer
 import (
    "testing"
 
-   "monkey/token"
+   "monkey-lang/token"
 )
 
 func TestNextToken(t *testing.T) {
 	input := `=+(){},;`
 
 	tests := []struct {
-		expectedType token.tokenType
+		expectedType token.TokenType
 		expectedLiteral string
 	}{
 		{token.ASSIGN, "="},
@@ -24,7 +24,7 @@ func TestNextToken(t *testing.T) {
 		{token.EOF, ""},
 	}
 
-	l := New(input)
+	l := new(input)
 
 	for i, tt := range tests {
 		tok := l.NextToken()
@@ -32,6 +32,11 @@ func TestNextToken(t *testing.T) {
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 			i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+			i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
