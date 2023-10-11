cf927455aba5596f3fab7260a7b851c907ddff2d
neshamon <jmmatthews@proton.me>
2023-10-11 00:07:44 -0500

    Add not equal & equal tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,13 +9,25 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch // Saves the current char in this variable
+			l.readChar()
+			literal := string(ch) + string(l.ch) // appends the current char to the next
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '!' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
@@ -60,6 +72,14 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 } // NextToken() tests l.ch against each case and returns the
 // according type and stringified l.ch
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+} // peekChar will return the value of the next char if not null
 
 func (l *Lexer) readNumber() string {
 	position := l.position
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -20,7 +20,11 @@ func TestNextToken(t *testing.T) {
             return true;
         } else {
             return false;
-        }`
+        }
+
+        10 == 10;
+        10 != 9;
+        `
 
 	tests := []struct {
 		expectedType token.TokenType
