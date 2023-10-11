30a8d4b7720c50fbeab483acb67161492f14d0ef
neshamon <jmmatthews@proton.me>
2023-10-10 22:07:45 -0500

    Add notes

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,21 +9,27 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
+		// If nothing has been read
+		// or EOF has been reached
+		// ch is set to 0
 		l.ch = 0
 	} else {
+		// Otherwise ch is set to the next character
 		l.ch = l.input[l.readPosition]
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
 }
 
-/* position & readPosition access characters in input by using the characters
- as an index.
+/*
+   position & readPosition access characters in input by using the characters
+   as an index.
 
    Why there are two positions is so both the input of the current
    character and the subsequent one can be inspected. position describes the
